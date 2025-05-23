2fb8df8d9e04bbb4bda2d36c31875e7dd0711660
David Shore <david@foggymedia.com>
2025-05-23 15:37:41 +0200

    -

--- api/config/database.js
+++ api/config/database.js
@@ -4,16 +4,19 @@ import dotenv from "dotenv";
 
 dotenv.config(); // Load environment variables from .env file
 
-const sequelize = new Sequelize({
-  dialect: "postgres",
-  dialectModule: pg,
-  host: process.env.PG_HOST || "localhost",
-  // port: process.env.DB_PORT || 5432,
-  database: process.env.PGDATABASE || "Sentinel",
-  username: process.env.PGUSER || "your_pg_user",
-  password: process.env.PGPASSWORD || "your_pg_password",
-  logging: false,
-});
+const sequelize = new Sequelize(process.env.DATABASE_URL);
+
+// const sequelize = new Sequelize({
+//   dialect: "postgres",
+//   dialectModule: pg,
+//   url: process.env.DATABASE_URL
+//   host: process.env.PG_HOST || "localhost",
+//   port: process.env.DB_PORT || 5432,
+//   database: process.env.PGDATABASE || "Sentinel",
+//   username: process.env.PGUSER || "your_pg_user",
+//   password: process.env.PGPASSWORD || "your_pg_password",
+//   logging: false,
+// });
 
 sequelize
   .authenticate()
