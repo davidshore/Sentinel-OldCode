7e36cd7fa4e709c9b86b81de5a6c31b539f648ad
David Shore <david@foggymedia.com>
2025-05-23 14:46:55 +0200

    -

--- api/routes.js
+++ api/routes.js
@@ -1,30 +1,30 @@
 import express from "express";
-import {
-  loginUser,
-  registerUser,
-  getMe,
-  updateMe,
-  deleteMe,
-  forgotPassword,
-  resetPassword,
-} from "./controllers/authController.js";
+// import {
+//   loginUser,
+//   registerUser,
+//   getMe,
+//   updateMe,
+//   deleteMe,
+//   forgotPassword,
+//   resetPassword,
+// } from "./controllers/authController.js";
 
-import { authenticateToken } from "./middlewares/authenticateToken.js";
+// import { authenticateToken } from "./middlewares/authenticateToken.js";
 
 const router = express.Router();
 
 // Auth
-router.post("/auth/register", registerUser);
-router.post("/auth/login", loginUser);
+// router.post("/auth/register", registerUser);
+// router.post("/auth/login", loginUser);
 
-// User profile
-router.get("/auth/me", authenticateToken, getMe);
-router.patch("/auth/me", authenticateToken, updateMe);
-router.delete("/auth/me", authenticateToken, deleteMe);
+// // User profile
+// router.get("/auth/me", authenticateToken, getMe);
+// router.patch("/auth/me", authenticateToken, updateMe);
+// router.delete("/auth/me", authenticateToken, deleteMe);
 
-// Password reset
-router.post("/auth/forgot-password", forgotPassword);
-router.post("/auth/reset-password", resetPassword);
+// // Password reset
+// router.post("/auth/forgot-password", forgotPassword);
+// router.post("/auth/reset-password", resetPassword);
 
 // index
 router.get("/", (req, res) => res.send("Express on Vercel"));
